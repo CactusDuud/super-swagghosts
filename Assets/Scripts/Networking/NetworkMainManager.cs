89fb461837dd712ff77bf21817ab7a7be79c7a0a
Cactuus <kingcactaurx@gmail.com>
2022-04-26 17:49:23 -0700

    Fleshing out the main menu

--- Assets/Scripts/Networking/NetworkMainManager.cs
+++ Assets/Scripts/Networking/NetworkMainManager.cs
@@ -1,12 +1,15 @@
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 using Photon.Pun;
 using Photon.Realtime;
 
 public class NetworkMainManager : MonoBehaviourPunCallbacks
 {
+    [SerializeField] private Button _joinButton;
     /// <summary> TextMeshPro InputField for players to write their desired room name into. </summary>
     [SerializeField] private TMP_InputField _roomName;
+    [SerializeField] private byte _maxPlayersPerRoom = 5;
 
     private string _gameVersion = "0.0.1";
 
@@ -16,6 +19,13 @@ public class NetworkMainManager : MonoBehaviourPunCallbacks
     {
         // Ensures that all clients load levels when the parent does.
         PhotonNetwork.AutomaticallySyncScene = true;
+        Debug.Log($"{name}: Automatic Scene Synching enabled");
+    }
+
+    private void Start()
+    {
+        // Disable join button until connected
+        _joinButton.enabled = false;
 
         // Immediately try to connect to PUN servers.
         // Also save connection status for 
@@ -30,30 +40,57 @@ public class NetworkMainManager : MonoBehaviourPunCallbacks
     public override void OnConnectedToMaster()
     {
         Debug.Log($"{name}: Connected to Photon Cloud (region: {PhotonNetwork.CloudRegion}).");
-
-        // TODO: Toggle which buttons are available
+        
+        // Enable the join button to allow players to join a room
+        _joinButton.enabled = true;
     }
 
     public override void OnDisconnected(DisconnectCause cause)
     {
-        // TODO: Toggle which buttons are available
+        Debug.Log($"{name}: Disconnected from Photon Cloud\nCause: {cause}");
+    }
 
-        Debug.Log($"{name}: Disconnected from room\nCause: {cause}");
+    public override void OnJoinedLobby()
+    {
+        Debug.Log($"{name}: Joined lobby for Super Swagghosts.");
     }
 
     public override void OnCreatedRoom()
     {
         Debug.Log($"{name}: Created new room \"{_roomName}\"");
     }
+
+    public override void OnJoinedRoom()
+    {
+        Debug.Log($"{name}: Joined room \"{_roomName}\"");
+    }
     #endregion
 
-    /// <summary> Creates a room on the Photon Network for </summary>
+
+    /// <summary> Joins a room on the Photon Network with the provided name, or creates one if it doesn't exist. </summary>
     public void JoinRoom()
     {
         if (PhotonNetwork.IsConnected)
         {
             Debug.Log($"{name}: Joining room \"{_roomName.text}\"...");
-            PhotonNetwork.JoinOrCreateRoom(_roomName.text, new RoomOptions(), null);
+
+            // Configure settings for the room
+            // Ngl this is barely important it just prevents an error
+            RoomOptions _roomConfig = new RoomOptions
+            {
+                MaxPlayers = _maxPlayersPerRoom
+            };
+
+            PhotonNetwork.JoinOrCreateRoom(_roomName.text, _roomConfig, null);
+            //TODO: Load the room menu scene
         }
+    }
+
+    /// <summary> Close the application. </summary>
+    public void QuitGame()
+    {
+        Debug.Log($"{name}: Closing application...");
+
+        Application.Quit();
     }
 }
