17851c4c592078fff77a53a2e6f35a45e77a69d3
huaac <huaac1@uci.edu>
2022-04-27 20:16:43 -0700

    updated player health

--- Assets/Scripts/Health/ParentHealth.cs
+++ Assets/Scripts/Health/ParentHealth.cs
@@ -3,37 +3,38 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class ParentHealth : MonoBehaviour
+public abstract class ParentHealth : MonoBehaviour
 {
-    public Slider slider;
-    public Gradient gradient;
-    public Image fill;
+    protected int curr_health;
+    // public Slider slider;
+    // public Gradient gradient;
+    // public Image fill;
 
-    // Each enemy can set their max health
-    public void SetMaxHealth(int health)
-    {
-        slider.maxValue = health;
-        slider.value = health;
+    // // Each enemy can set their max health
+    // public void SetMaxHealth(int health)
+    // {
+    //     slider.maxValue = health;
+    //     slider.value = health;
 
-        fill.color = gradient.Evaluate(1f);
-    }
+    //     fill.color = gradient.Evaluate(1f);
+    // }
 
-    // resets the health to show change
-    public void SetHealth(int health)
-    {
-        slider.value = health;
-        fill.color = gradient.Evaluate(slider.normalizedValue);
-    }
+    // // resets the health to show change
+    // public void SetHealth(int health)
+    // {
+    //     slider.value = health;
+    //     fill.color = gradient.Evaluate(slider.normalizedValue);
+    // }
 
-    virtual public void TakeDamage(int damage)
-    {
-        // CurrentHealth -= damage;
-        // ChangeHealthBar();
+    protected virtual void TakeDamage(int damage) {}
+    // {
+    //     CurrentHealth -= damage;
+    //     ChangeHealthBar();
 
-        // if (CurrentHealth <= 0)
-        // {
-        //     Die();
-        // }
-    }
+    //     if (CurrentHealth <= 0)
+    //     {
+    //         Die();
+    //     }
+    // }
 
 }
--- Assets/Scripts/Health/PlayerHealth.cs
+++ Assets/Scripts/Health/PlayerHealth.cs
@@ -5,13 +5,34 @@ using UnityEngine;
 public class PlayerHealth : ParentHealth
 {
 
-    protected int curr_health;
-
     [SerializeField] protected int max_health = 100; // can be overrided for variation
-    
+
+
+    // sets max health to current health
     void Awake()
     {
         curr_health = max_health;
+        Debug.Log(curr_health);
+    }
+
+    // subtract damage from current health
+    protected override void TakeDamage(int damage)
+    {
+        curr_health -= damage;
+    }
+
+    //when collides with a ghost, player's health falls to 0 and player can't move
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Ghost")
+        {
+            if(curr_health != 0)
+            {
+                TakeDamage(100);
+                GetComponent<ParentController>().enabled = false;
+                Debug.Log(curr_health);
+            }
+        }
     }
     
 }
