381e3f65d8708437e9322e33c4231ddb3c856908
Mindy Jun <mindyjvn@gmail.com>
2022-06-04 14:39:13 -0700

    EndScreen fix?
    using Text instead of GameObject to see if it makes a diff

--- Assets/EndScreenMisc.cs
+++ Assets/EndScreenMisc.cs
@@ -1,15 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class EndScreenMisc : MonoBehaviour
 {
-    public GameObject txt;
+    public Text txt;
 
     void Start() 
     {
-        txt.GetComponent<UnityEngine.UI.Text>().text = TempGameLoop.winner;
+        txt.text = TempGameLoop.winner;
     }
 
     public void toMainMenu() 
