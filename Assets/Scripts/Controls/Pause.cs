6a60993d450d57d09172e372b8b41bc3d9fe11ef
philipgergis <philipgergis@gmail.com>
2022-06-04 07:05:57 -0700

    try new pause

--- Assets/Scripts/Controls/Pause.cs
+++ Assets/Scripts/Controls/Pause.cs
@@ -1,28 +1,37 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
 
-public class Pause : MonoBehaviour
+public class Pause : MonoBehaviourPunCallbacks
 {
     public GameObject optionsMenu;
-    public GameObject dimImage;
+    public GameObject backButton;
     public GameObject optionsButton;
 
+
     public void PauseGame()
+    {
+        this.photonView.RPC("PauseGameRPC", RpcTarget.All);
+    }
+
+    [PunRPC]
+    private void PauseGameRPC()
     {
         if (!optionsMenu.gameObject.activeSelf)
         {
             Time.timeScale = 0f;
             optionsMenu.gameObject.SetActive(true);
-            dimImage.gameObject.SetActive(true);
-            optionsButton.gameObject.SetActive(true);
+            backButton.gameObject.SetActive(true);
+            optionsButton.gameObject.SetActive(false);
         }
         else
         {
             Time.timeScale = 1f;
             optionsMenu.gameObject.SetActive(false);
-            dimImage.gameObject.SetActive(false);
-            optionsButton.gameObject.SetActive(false);
+            backButton.gameObject.SetActive(false);
+            optionsButton.gameObject.SetActive(true);
         }
     }
 }
