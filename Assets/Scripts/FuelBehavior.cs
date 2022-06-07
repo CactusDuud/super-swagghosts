dac00b3be18061fb506080d6a5e66e7c794f8780
Cactuus <kingcactaurx@gmail.com>
2022-06-07 16:34:32 -0700

    Update oil pickup

--- Assets/Scripts/FuelBehavior.cs
+++ Assets/Scripts/FuelBehavior.cs
@@ -4,6 +4,21 @@ using UnityEngine;
 
 public class FuelBehavior : MonoBehaviour
 {
+    [SerializeField] private float _wiggleDistance = 1;
+    [SerializeField] private float _wiggleSpeed = 1;
+    private GameObject _sprite;
+
+    private void Awake()
+    {
+        _sprite = GetChild();
+    }
+     
+    private void Update()
+    {
+        float yPosition = Mathf.Sin(Time.time * _wiggleSpeed) * _wiggleDistance;
+        _sprite.transform.localPosition = new Vector3(0, yPosition, 0);
+    }
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if(collision.tag == "Player")
